4fcfa2cbbe4192c195ac72c27e760e77bc8790ef
Sam Maier <sam@sammaier.ca>
2017-08-09 12:29:49 -0400

    Service being published

--- app/src/main/java/com/uwaterlooece458/projectece458/ReceiveKeyActivity.java
+++ app/src/main/java/com/uwaterlooece458/projectece458/ReceiveKeyActivity.java
@@ -1,16 +1,24 @@
 package com.uwaterlooece458.projectece458;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class ReceiveKeyActivity extends AppCompatActivity {
+    BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    public static final java.util.UUID MY_UUID
+            = java.util.UUID.fromString("B10E7007-CCD4-BBD7-1AAA-5EC000000017");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +41,8 @@ public class ReceiveKeyActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        new AcceptThread().start();
     }
 
     private void saveIncomingKey(String filename, String contents) {
@@ -46,6 +56,57 @@ public class ReceiveKeyActivity extends AppCompatActivity {
             outputStream.close();
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private class AcceptThread extends Thread {
+        private final BluetoothServerSocket mmServerSocket;
+
+        public AcceptThread() {
+            // Use a temporary object that is later assigned to mmServerSocket
+            // because mmServerSocket is final.
+            BluetoothServerSocket tmp = null;
+            try {
+                // MY_UUID is the app's UUID string, also used by the client code.
+                tmp = mBluetoothAdapter.listenUsingRfcommWithServiceRecord("AcceptKeys", MY_UUID);
+            } catch (IOException e) {
+                Log.e("BLUETOOTHSECURITY", "Socket's listen() method failed", e);
+            }
+            mmServerSocket = tmp;
+        }
+
+        public void run() {
+            BluetoothSocket socket = null;
+            // Keep listening until exception occurs or a socket is returned.
+            while (true) {
+                try {
+                    socket = mmServerSocket.accept();
+                } catch (IOException e) {
+                    Log.e("BLUETOOTHSECURITY", "Socket's accept() method failed", e);
+                    break;
+                }
+
+                if (socket != null) {
+                    // A connection was accepted. Perform work associated with
+                    // the connection in a separate thread.
+                    //manageMyConnectedSocket(socket);
+                    try {
+                        mmServerSocket.close();
+                    } catch (IOException e) {
+                        Log.e("BLUETOOTHSECURITY", "Socket's close() method failed", e);
+                    }
+                    break;
+                }
+            }
+        }
+
+        // Closes the connect socket and causes the thread to finish.
+        public void cancel() {
+            try {
+                mmServerSocket.close();
+            } catch (IOException e) {
+                Log.e("BLUETOOTHSECURITY", "Could not close the connect socket", e);
+            }
         }
     }
 }
